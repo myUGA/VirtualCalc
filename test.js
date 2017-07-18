396a6d111ac64284ed415373ec82f3992ed7fda7
mbam <imaginerationally@gmail.com>
2017-07-18 18:53:17 +0000

    adding index and test.js

--- test.js
+++ test.js
@@ -11,4 +11,6 @@
 
 // // Console will print the message
 // console.log('Server running at http://127.0.0.1:8081/');
-console.log('ssssssng at http://127.0.0.1:8081/');
+function printCalculation(){
+	alert('Calculation');
+}
